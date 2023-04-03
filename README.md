905cb8ce509ba6e16e6eb4ac911b5fd5fa46008f
evanboyle <evan@pulumi.com>
2023-04-02 20:42:32 -0700

    initial implementation of the aws importer

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pulumi-insights-import-demo
-Import infrastructure managed outside of Pulumi IaC into Pulumi Insights
+Import infrastructure managed outside of Pulumi IaC into Pulumi Insights. Currently supports AWS only.
+
+To run:
+
+1. Clone this repo
+2. Run `pulumi stack init <organization/aws-account-name>`
+3. Set the appropriate AWS region you'd like to import resources from `export AWS_REGION=us-west-2`
+4. Run the importer `go run main.go
+
+TODO:
+- Document how to hook this up to Pulumi Deployments + OIDC so this can be 1-click from the console
+- Unfortunately this doesn't work end to end until https://github.com/pulumi/pulumi-aws-native/issues/854 is fixed. 
+  In the meantime, I can call `pulumi import` one resource at a time which is very slow and frustrating...
---
+++ pulumi-insights-import/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/pulumi/pulumi/pkg/v3/codegen/dotnet"
+	pschema "github.com/pulumi/pulumi/pkg/v3/codegen/schema"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudcontrolapi"
+)
+
+type importFile struct {
+	NameTable map[string]resource.URN `json:"nameTable"`
+	Resources []importSpec            `json:"resources"`
+}
+
+type importSpec struct {
+	Type              string   `json:"type"`
+	Name              string   `json:"name"`
+	ID                string   `json:"id"`
+	Parent            string   `json:"parent"`
+	Provider          string   `json:"provider"`
+	Version           string   `json:"version"`
+	PluginDownloadURL string   `json:"pluginDownloadUrl"`
+	Properties        []string `json:"properties"`
+}
+
+func main() {
+
+	/*
+		3. turn those type tokens into cloud control api resource type tokens
+		4. construct a pulumi import resource file
+		5. shell out and run pulumi import.
+	*/
+	pkgSpec, err := getAWSNativeSchema()
+	if err != nil {
+		panic(err)
+	}
+
+	csharpRaw := pkgSpec.Language["csharp"]
+	csharpInfo := dotnet.CSharpPackageInfo{}
+	if err := json.Unmarshal(csharpRaw, &csharpInfo); err != nil {
+		panic(err)
+	}
+
+	imports := importFile{
+		Resources: []importSpec{},
+	}
+
+	sess, err := session.NewSession()
+	if err != nil {
+		panic(err)
+	}
+	client := cloudcontrolapi.New(sess)
+	totalResources := 0
+
+	for k := range pkgSpec.Resources {
+		parts := strings.Split(k, ":")
+
+		namespace, ok := csharpInfo.Namespaces[parts[1]]
+		if !ok {
+			namespace = parts[1]
+		}
+		cloudControlType := fmt.Sprintf("AWS::%s::%s", namespace, parts[2])
+		fmt.Println(cloudControlType)
+
+		resourceNumber := 0
+		params := &cloudcontrolapi.ListResourcesInput{
+			MaxResults: aws.Int64(100),
+			TypeName:   aws.String(cloudControlType),
+		}
+		err = client.ListResourcesPages(params,
+			func(page *cloudcontrolapi.ListResourcesOutput, lastPage bool) bool {
+				for _, r := range page.ResourceDescriptions {
+					resourceNumber++
+					totalResources++
+					if r.Identifier != nil {
+						imports.Resources = append(imports.Resources, importSpec{
+							ID:   *r.Identifier,
+							Type: k,
+							Name: fmt.Sprintf("%s%s%d", namespace, parts[2], resourceNumber),
+						})
+					}
+				}
+				return true
+			})
+
+		// just print out errors as info for now
+		// as there are some resources that don't support ListResources
+		// or have special auth requirements.
+		// TODO -- handle rate limiting errors
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+	fmt.Printf("Total resources: %d", totalResources)
+
+	err = writeImportFile(imports)
+	if err != nil {
+		panic(err)
+	}
+
+	err = callPulumiImport()
+	if err != nil {
+		panic(err)
+	}
+}
+
+// download https://raw.githubusercontent.com/pulumi/pulumi-aws-native/master/provider/cmd/pulumi-resource-aws-native/schema.json
+// and parse it into a pschema.PackageSpec
+func getAWSNativeSchema() (*pschema.PackageSpec, error) {
+	schemaURL := "https://raw.githubusercontent.com/pulumi/pulumi-aws-native/master/provider/cmd/pulumi-resource-aws-native/schema.json"
+
+	resp, err := http.Get(schemaURL)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	var schema pschema.PackageSpec
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(resp.Body)
+	respByte := buf.Bytes()
+	if err := json.Unmarshal(respByte, &schema); err != nil {
+		return nil, err
+	}
+
+	return &schema, nil
+}
+
+// write import file to disk
+func writeImportFile(imports importFile) error {
+	// write the import file to disk
+	importFile, err := json.MarshalIndent(imports, "", "    ")
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile("import.json", importFile, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func callPulumiImport() error {
+	// run pulumi import
+	cmd := exec.Command("pulumi", "import", "-p", "1", "-f", "import.json", "--yes")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
